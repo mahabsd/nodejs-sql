914c22027d28c3cf573d74a62fad9af884de62fb
Mahabsd <Mahabsd@gmail.com>
2021-06-15 09:26:47 +0100

    join users with fruits

--- users.js
+++ users.js
@@ -22,4 +22,13 @@ router.get('/adduser', (req, res) => {
         res.send('user added')
     })
 })
+router.get('/joinUsersFruits',(req,res)=>{
+    let sql = 'SELECT users.name AS user ,fruits.name AS fruitid FROM users JOIN fruits ON users.fruitid = fruits.id  '
+    const db = req.app.get('db');
+db.query(sql, (err, result)=>{
+    if (err) throw err;
+res.send(result)
+})
+
+})
 module.exports = router;
